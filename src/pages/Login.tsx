473c2662496a57c5d3188284e91ba34d185d767d
Masum Billah <masumbillah2062003@gmail.com>
2025-03-16 15:06:14 +0600

    first commit

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,48 @@
-
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/hook";
+import { setUser } from "../redux/features/auth/authSlice";
+import { veryfyToken } from "../utils/veryfyToken";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>this is login page</h1>
-        </div>
-    );
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0001",
+      password: "admin123",
+    },
+  });
+
+  const [login, { data, error }] = useLoginMutation();
+
+  const onSubmit = async (data: { userId: string; password: string }) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = veryfyToken(res.data.accessToken);
+
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  };
+
+  console.log("data =>", data);
+  console.log("error =>", error);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID: </label>
+        <input type="text" id="id" {...register("userId")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password: </label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
+  );
 };
 
 export default Login;
