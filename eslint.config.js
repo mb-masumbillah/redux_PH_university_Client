a97da3d577ea2efc48741467efd51680ae7ed070
Masum Billah <masumbillah2062003@gmail.com>
2025-05-08 21:44:26 +0600

    first commit

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
-       "eslint-disable @typescript-eslint/no-explicit-any": "off" 
+      "eslint-disable @typescript-eslint/no-explicit-any": "off"
     },
   },
 )
